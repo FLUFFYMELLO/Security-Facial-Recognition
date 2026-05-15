a7435666508bbfa60b09a71397e7f720e8600bc0
lawrence <renrenmicrosoft@gmail.com>
2026-05-16 01:57:52 +0800

    working face detection

--- Main.py
+++ Main.py
@@ -1,6 +1,18 @@
 import cv2
 
 Camera = cv2.VideoCapture(0)
+detector = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
 while True:
     ret, frame = Camera.read()
+    Grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    Faces = detector.detectMultiScale(Grayscale, 1.5, 5)
+    for (x,y,w,h) in Faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (50, 50, 255), 2)
+    cv2.imshow("Frame",frame)
+    key = cv2.waitKey(1)
+    if key == ord('p'):
+        break
+
+Camera.release()
+cv2.destroyAllWindows()
